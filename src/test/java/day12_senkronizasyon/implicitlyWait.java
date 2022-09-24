6d4d15bdb4dca09191810abd7c3d2e61c0804685
Sumeyye Guler <sumeyyeguler0602@gmail.com>
2022-09-24 21:03:40 +0300

    24.09.2022

---
+++ src/test/java/day12_senkronizasyon/implicitlyWait.java
@@ -0,0 +1,18 @@
+package day12_senkronizasyon;
+
+public class implicitlyWait {
+    /*
+    implicitly wait;
+    sayfadaki tüm öğeler için global bir zaman aşmıdır(timeout).
+    sayfadaki senkronizasyon ile ilgili problemleri yüzde 90 çözer.
+    sayfanın yüklenmesi ve sayfadaki web elementlerin bulunması noktasında global bir bekleme süresiydi.
+    sayfanın yüklenmesi ve sayfadaki web elementlerin bulunması 15 sn den fazla beklemeye gerek yok,
+    15 sn de olmuyorsa bir problem var demektir bana dön diyoruz.
+    mesela 15 sn ayarlandıysa, her bir locste işleminde 15 er saniye zaman ayırır.
+
+    explicitly wait; şartlı beklemedir.
+    sadece bir işlem için tanımlanır.
+
+    fluent wait;belirli periyotlarla çalışıyor,aralıklı. zaman dilimleri vardır 3 sn de bir.
+     */
+}
